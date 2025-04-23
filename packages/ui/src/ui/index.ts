1d97ab09c2a18b0581d175cbbb29848c1c95b597
serudda <sergioruizdavila@gmail.com>
2025-04-22 20:35:48 -0500

    feat(ui): export additional components from index file, including Inputs and Spinner

--- packages/ui/src/ui/index.ts
+++ packages/ui/src/ui/index.ts
@@ -1,4 +1,7 @@
+export * from './Avatar/Avatar';
 export * from './Button/Button';
 export * from './Button/LoadingButton';
 export * from './Command/Command';
+export * from './Inputs';
 export * from './Overlays';
+export * from './Spinner/Spinner';
