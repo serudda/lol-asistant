622b1b356a96fd4cab9a76d2134f72b9043eb563
serudda <sergioruizdavila@gmail.com>
2025-05-02 15:47:16 -0500

    refactor(cron-scripts): remove unused transformJsonValue function to streamline code

--- packages/cron-scripts/src/scripts/updateChampionStats/common/transformJsonValue.ts
+++
@@ -1,6 +0,0 @@
-import type { JsonValue } from '@prisma/client/runtime/library';
-
-export const transformJsonValue = (value: JsonValue | null): Record<string, any> | undefined => {
-  if (value === null) return undefined;
-  return value as Record<string, any>;
-};
