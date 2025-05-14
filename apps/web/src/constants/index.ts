80a3827f7943c29ec5ca0902b4762da8e92ebcf7
serudda <sergioruizdavila@gmail.com>
2025-05-14 13:29:13 -0500

    refactor(web): remove lolChampionRole constants file and update exports

--- apps/web/src/constants/index.ts
+++ apps/web/src/constants/index.ts
@@ -1,2 +1,1 @@
-export * from './lolChampionRole';
 export * from './rankTier';
--- apps/web/src/constants/lolChampionRole.ts
+++
@@ -1,9 +0,0 @@
-import { LoLChampionRole } from '@prisma/client';
-
-export const roleOptions = [
-  { value: LoLChampionRole.top, label: 'Top', imageUrl: '/assets/roles/lol-role-top.svg' },
-  { value: LoLChampionRole.jungle, label: 'Jungle', imageUrl: '/assets/roles/lol-role-jungle.svg' },
-  { value: LoLChampionRole.mid, label: 'Mid', imageUrl: '/assets/roles/lol-role-mid.svg' },
-  { value: LoLChampionRole.adc, label: 'ADC', imageUrl: '/assets/roles/lol-role-adc.svg' },
-  { value: LoLChampionRole.support, label: 'Support', imageUrl: '/assets/roles/lol-role-support.svg' },
-];
