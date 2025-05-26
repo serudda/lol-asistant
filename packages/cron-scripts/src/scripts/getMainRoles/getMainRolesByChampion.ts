6c06045cb2e7769be8844c0bee2fa4be360d63e7
serudda <sergioruizdavila@gmail.com>
2025-05-26 13:36:36 -0500

    feat(cron-scripts): enhance getHtmlFromPage to accept custom headers for improved web scraping

--- packages/cron-scripts/src/scripts/getMainRoles/getMainRolesByChampion.ts
+++ packages/cron-scripts/src/scripts/getMainRoles/getMainRolesByChampion.ts
@@ -18,7 +18,14 @@ export const getMainRolesByChampion = async (slug: string, rank: RankTier): Prom
     // Fetch all raw main roles from League of Graphs HTML page by web scraping
     console.log(`[getMainRolesByChampion] Web scraping main roles for: ${slug} (${rank})`);
     const url = `${BASE_URL}/champions/stats/${slug}/${rank}`;
-    const html = await getHtmlFromPage(url);
+    const headers = {
+      'User-Agent':
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
+      Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+      'Accept-Language': 'en-US,en;q=0.9',
+      Referer: 'https://www.leagueofgraphs.com/',
+    };
+    const html = await getHtmlFromPage(url, headers);
 
     // Extract main roles from HTML
     const mainRoles = extractDataFromHtml(html);
