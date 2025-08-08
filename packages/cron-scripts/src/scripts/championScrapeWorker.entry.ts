83851feee2189259f4b00b196455314e445f69cb
serudda <sergioruizdavila@gmail.com>
2025-08-07 19:08:49 -0500

    fix: update script documentation

--- packages/cron-scripts/src/scripts/championScrapeWorker.entry.ts
+++ packages/cron-scripts/src/scripts/championScrapeWorker.entry.ts
@@ -3,8 +3,7 @@ import championScrape from '../championScrape/championScrape.worker';
 export default championScrape;
 
 /**
- * Run the script `pnpm script:run
- * championScrapeWorker.entry`
+ * Run the script `pnpm script:run championScrapeWorker.entry`
  *
  * This script starts the champion-scrape worker.
  */
